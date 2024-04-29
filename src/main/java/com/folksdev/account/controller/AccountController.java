11feefe7d630f0d6fc62afc0e009be644f94cdbb
Ceyda Selamet <ceydaselamet@gmail.com>
2024-04-30 01:00:03 +0300

    Exception Handling have been improved.

--- src/main/java/com/folksdev/account/controller/AccountController.java
+++ src/main/java/com/folksdev/account/controller/AccountController.java
@@ -3,6 +3,7 @@ package com.folksdev.account.controller;
 import com.folksdev.account.dto.AccountDto;
 import com.folksdev.account.dto.CreateAccountRequest;
 import com.folksdev.account.service.AccountService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,7 +18,7 @@ public class AccountController {
     }
 
     @PostMapping
-    public ResponseEntity<AccountDto> createAccount(@RequestBody CreateAccountRequest request){
+    public ResponseEntity<AccountDto> createAccount(@Valid @RequestBody CreateAccountRequest request){
         return ResponseEntity.ok(accountService.createAccount(request));
     }
 }
--- src/main/java/com/folksdev/account/dto/CreateAccountRequest.kt
+++ src/main/java/com/folksdev/account/dto/CreateAccountRequest.kt
@@ -1,9 +1,13 @@
 package com.folksdev.account.dto
 
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotBlank
 import java.math.BigDecimal
 
 class CreateAccountRequest(
-
+    @field:NotBlank(message = "Id boş geçilemez.")
     val customerId: String,
+
+    @field:Min(value=0, message = "0'dan küçük değer girilemez.")
     val initialCredit: BigDecimal
 )
